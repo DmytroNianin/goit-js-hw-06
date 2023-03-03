46f57716e5faefb9fdfee670e69fcaff532806da
DmytroNianin <108230718+DmytroNianin@users.noreply.github.com>
2023-03-03 18:06:46 +0100

    hw-js-06

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const categoriesEl = document.querySelectorAll("#categories > .item");
-console.log("Number of categories: ", categoriesEl.length);
+const categoriesEl = document.querySelectorAll('#categories > .item');
+console.log('Number of categories: ', categoriesEl.length);
 
-categoriesEl.forEach((element) => {
-  console.log("Category: ", element.querySelector("h2").textContent);
-  console.log("Elements: ", element.querySelectorAll("li").length);
+categoriesEl.forEach(element => {
+  console.log('Category: ', element.firstElementChild.textContent);
+  console.log('Elements: ', element.querySelectorAll('li').length);
 });
